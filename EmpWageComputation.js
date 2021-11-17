70053ec2050cfad1a6e72c9c4f47c4ca2e0e9e6e
unknown <khomanesuraj8@gmail.com>
2021-11-17 19:15:38 +0530

    calculate emp monthly hours

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -4,6 +4,7 @@ const IS_FULL_TIME =2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR =20;
+const NUM_OF_WORKING_DAY = 2;
 
 //fuction to check part or full time
 function getWorkingHours(empCheck){
@@ -19,10 +20,15 @@ function getWorkingHours(empCheck){
     }
 }
 let empHrs = 0;
-//get random values
-let empCheck = Math.floor(Math.random()*10) % 3;
-//calculate emp hrs
-empHrs = getWorkingHours(empCheck);
+
+//calculate total working hours
+for (let day = 0; day < NUM_OF_WORKING_DAY; day++){
+    //get random values
+    let empCheck = Math.floor(Math.random()*10) % 3;
+    //calculate emp hrs
+    empHrs += getWorkingHours(empCheck);
+}
+
 //calculate emp wage
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage : "+empWage);
+console.log("Total Hrs : " + empHrs+ "\nEmployee Monthly Wage : "+empWage);
