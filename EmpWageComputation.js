7212623a49a1caf93c7fa52a58a6b6cbc35237e8
unknown <khomanesuraj8@gmail.com>
2021-11-17 19:34:29 +0530

    calculate emp wage till total working days or total working hours

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -4,7 +4,8 @@ const IS_FULL_TIME =2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR =20;
-const NUM_OF_WORKING_DAY = 2;
+const NUM_OF_WORKING_DAY = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 //fuction to check part or full time
 function getWorkingHours(empCheck){
@@ -19,16 +20,20 @@ function getWorkingHours(empCheck){
             return 0;
     }
 }
-let empHrs = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
 
 //calculate total working hours
-for (let day = 0; day < NUM_OF_WORKING_DAY; day++){
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+       totalWorkingDays <= NUM_OF_WORKING_DAY){
+    //increment working days
+    totalWorkingDays++;
     //get random values
     let empCheck = Math.floor(Math.random()*10) % 3;
     //calculate emp hrs
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
 //calculate emp wage
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Total Hrs : " + empHrs+ "\nEmployee Monthly Wage : "+empWage);
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Days : "+ totalWorkingDays +"\nTotal Hrs : " + totalEmpHrs+ "\nEmployee Monthly Wage : "+empWage);
