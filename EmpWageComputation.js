3e1c5d1d4eaed3a8664ac11aa74423f1ff1c203a
unknown <khomanesuraj8@gmail.com>
2021-11-17 18:04:08 +0530

    check employee present or not

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,2 +1,11 @@
 console.log("Welcome to Employee Wage Calculation");
+//UC1
+const IS_ABSENT = 0
 
+let empCheck = Math.floor(Math.random()*10) % 2;
+if(empCheck == IS_ABSENT){
+    console.log("Employee is absent");
+}
+else{
+    console.log("Employee is present");
+}
